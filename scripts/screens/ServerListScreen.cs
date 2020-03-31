2ce7d5eb0186aed9a876d48b81cb7cd46dead0a8
MiniDigger <admin@minidigger.me>
2020-03-31 17:01:58 +0200

    pseudo server list

--- scripts/screens/ServerListScreen.cs
+++ scripts/screens/ServerListScreen.cs
@@ -1,3 +1,4 @@
+using System;
 using Godot;
 using Godotcraft.scripts.network.protocol;
 using Godotcraft.scripts.network.protocol.handshake;
@@ -5,13 +6,42 @@ using Godotcraft.scripts.network.protocol.status.client;
 
 namespace Godotcraft.scripts.screens {
 public class ServerListScreen : Control {
-	
+	private VBoxContainer serverList;
+	private RandomNumberGenerator random;
+
 	public override void _Ready() {
 		Button addButton = GetNode("CenterContainer/VBoxContainer/ButtonBar/Add") as Button;
 		Button backButton = GetNode("CenterContainer/VBoxContainer/ButtonBar/Back") as Button;
+		serverList = GetNode("CenterContainer/VBoxContainer/Servers") as VBoxContainer;
 
 		addButton?.Connect("pressed", this, nameof(onAdd));
 		backButton?.Connect("pressed", this, nameof(onBack));
+
+		random = new RandomNumberGenerator();
+		random.Randomize();
+
+		while (serverList?.GetChildCount() > 0) {
+			Node child = serverList.GetChild(0);
+			serverList.RemoveChild(child);
+			child.QueueFree();
+		}
+	}
+
+	private void addServer(String name, String motd1, String motd2, int current, int max) {
+		HBoxContainer hBoxContainer = new HBoxContainer();
+		// TODO server icon
+		TextureRect icon = new TextureRect();
+		NoiseTexture texture = new NoiseTexture();
+		texture.Width = 64;
+		texture.Height = 64;
+		texture.Noise = new OpenSimplexNoise();
+		texture.Noise.Seed = (int) random.Randi();
+		icon.Texture = texture;
+
+		Label label = new Label {Text = name + " (" + current + "/" + max + ")\n" + motd1 + "\n" + motd2};
+		hBoxContainer.AddChild(icon);
+		hBoxContainer.AddChild(label);
+		serverList.AddChild(hBoxContainer);
 	}
 
 	public void onAdd() {
@@ -27,6 +57,12 @@ public class ServerListScreen : Control {
 
 		Util.readPacket(stream);
 		Util.readPacket(stream);
+
+		addServer("Test", "Motd1", "Motd2", 0, 10);
+		addServer("Test", "Motd1", "Motd2", 0, 10);
+		addServer("Test", "Motd1", "Motd2", 0, 10);
+		addServer("Test", "Motd1", "Motd2", 0, 10);
+		addServer("Test", "Motd1", "Motd2", 0, 10);
 	}
 
 	public void onBack() {
